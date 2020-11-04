677c9ea7570ec181c34eee28926ffd38e2647b9e
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-03 18:34:46 -0800

    animals controller created

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,14 @@
+class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new
+
+  end
+end
